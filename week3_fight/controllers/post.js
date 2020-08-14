294644b3080e4c51a6073bed95c87aca14690980
Do-ho <hong7511059@koreatech.ac.kr>
2020-08-14 19:09:31 +0900

    Update week3_fight

--- week3_fight/controllers/post.js
+++ week3_fight/controllers/post.js
@@ -6,6 +6,22 @@ const axios = require("axios");
 const aggressiveEmotion = ["혐오", "분노"];
 const positiveEmotion = ["기쁨", "신뢰"];
 
+const AWS = require('aws-sdk');
+const fs = require('fs');
+const endpoint = new AWS.Endpoint('https://kr.object.ncloudstorage.com');
+const region = 'kr-standard';
+const access_key = 'OxzLSiVMnc7XeLWIaPNl'; //본인의 api 액세스 키
+const secret_key = '0PjANFs41zpT4NPASu6O75JxhF95i2FXiJCQqq8J'; //본인의 api 시크릿 키
+
+const S3 = new AWS.S3({
+  endpoint: endpoint,
+  region: region,
+  credentials: {
+      accessKeyId : access_key,
+      secretAccessKey: secret_key
+  }
+});
+
 exports.getPosts = async (req, res, next) => {
   try {
     let page = req.query.page || 0;
@@ -142,4 +158,36 @@ exports.postDeletePost = async (req, res, next) => {
     console.log(err);
     res.redirect("/posts");
   }
+};
+
+
+//그림 추가
+exports.addImage = async (req, res, next) => {
+  let url = './uploads/'+req.file.filename;
+
+  const bucket_name = 'relayest';
+  const local_file_path = url;
+
+
+  let object_name = Date.now()  + '.png';
+  let saveurl = 'http://heovyegsmorj4951114.cdn.ntruss.com/'+object_name+'?type=f&w=100&h=200&autorotate=false&faceopt=true&ttype=png&anilimit=1';
+  console.log(saveurl);
+  (async () => {
+
+  // create folder
+  await S3.putObject({
+      Bucket: bucket_name,
+      Key: object_name
+  }).promise();
+
+  // upload file
+  await S3.putObject({
+      Bucket: bucket_name,
+      Key: object_name,
+      ACL: 'public-read',
+      // ACL을 지우면 전체공개가 되지 않습니다.
+      Body: fs.createReadStream(local_file_path)
+  }).promise();
+
+  })();
 };
--- week3_fight/routes/user.js
+++ week3_fight/routes/user.js
@@ -4,12 +4,14 @@ const router = express.Router();
 
 const userController = require("../controllers/user");
 const authMiddleware = require("../middleware/is-auth");
+var multer = require('multer'); // express에 multer모듈 적용 (for 파일업로드)
+var upload = multer({ dest: 'uploads/' })
 
 //3주
 router.post("/getUser", authMiddleware.isLogin, userController.getUser);
 
 router.post("/updateUser", authMiddleware.isLogin, userController.updateUser);
 
-router.post("/edit-profile", authMiddleware.isLogin, userController.updateThumbnail);
+router.post("/edit-profile", upload.single('image'), authMiddleware.isLogin, userController.updateThumbnail);
 
 module.exports = router;
